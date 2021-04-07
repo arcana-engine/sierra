6cd0ea020096ed3551a322b66b05748e3bd391c3
Zakarum <zakarumych@ya.ru>
2021-04-07 22:21:14 +0300

    Working triangle demo

--- demo/src/main.rs
+++ demo/src/main.rs
@@ -1,81 +1,17 @@
-//! This crate contains example for sierra's code-generation feature.\
-//! It contains uniform structures and descriptor set layout.\
-//! Generated types allows creating descriptor set layout
-//! and descriptor sets with functions, no additional args aside from device is
-//! required.\
-//! Additionally it allows updating uniforms and descriptors binding in
-//! straightforward manner in single function call `instance.update(&input)`,\
-//! and then bind descriptor set to encoder.
+use {sierra::RenderPassInstance as _, tracing_subscriber::layer::SubscriberExt as _};
 
-use {
-    bumpalo::{collections::Vec as BVec, Bump},
-    sierra::RenderPassInstance as _,
-    tracing_subscriber::layer::SubscriberExt as _,
-};
-
-/// Descriptor set
-#[sierra::descriptors]
-pub struct Globals {
-    #[uniform]
-    #[stages(Vertex)]
-    pub camera_view: sierra::mat4,
-
-    #[uniform]
-    #[stages(Vertex)]
-    pub camera_proj: sierra::mat4,
-}
-
-#[sierra::descriptors]
-pub struct Object {
-    #[sampler]
-    pub s: sierra::Sampler,
-
-    #[sampled_image]
-    #[stages(Fragment)]
-    pub albedo: sierra::Image,
-
-    #[uniform]
-    #[stages(Vertex)]
-    pub transform: sierra::mat4,
-
-    #[uniform]
-    #[stages(Fragment)]
-    pub rgb: sierra::vec3,
-}
-
-/// Pipeline definition
 #[sierra::pipeline]
-pub struct Pipeline {
-    #[set]
-    globals: Globals,
-
-    #[set]
-    object: Object,
-}
+pub struct Pipeline;
 
 #[sierra::pass]
-#[subpass(color = target, depth = depth)]
+#[subpass(color = target)]
 pub struct Main {
     #[attachment(clear(bg), store(const sierra::Layout::Present))]
     target: sierra::Image,
-
     bg: sierra::ClearColor,
-
-    #[attachment(clear(const sierra::ClearDepth(1.0)))]
-    depth: sierra::Format,
 }
 
 fn main() -> eyre::Result<()> {
-    color_eyre::install()?;
-
-    tracing::subscriber::set_global_default(
-        tracing_subscriber::fmt()
-            .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
-            .pretty()
-            .finish()
-            .with(tracing_error::ErrorLayer::default()),
-    )?;
-
     let event_loop = winit::event_loop::EventLoop::new();
     let window = winit::window::Window::new(&event_loop)?;
 
@@ -94,17 +30,32 @@ fn main() -> eyre::Result<()> {
     )?;
 
     let shader_module = device.create_shader_module(sierra::ShaderModuleInfo {
-        code: std::include_bytes!("main.wgsl").to_vec().into_boxed_slice(),
+        code: br#"
+            [[stage(vertex)]]
+            fn vs_main([[builtin(vertex_index)]] in_vertex_index: u32) -> [[builtin(position)]] vec4<f32> {
+                const x = f32(i32(in_vertex_index) - 1);
+                const y = f32(i32(in_vertex_index & 1u) * 2 - 1);
+                return vec4<f32>(x, y, 0.0, 1.0);
+            }
+
+            [[stage(fragment)]]
+            fn fs_main() -> [[location(0)]] vec4<f32> {
+                return vec4<f32>(1.0, 0.0, 0.0, 1.0);
+            }
+        "#.to_vec().into_boxed_slice(),
         language: sierra::ShaderLanguage::WGSL,
     })?;
 
     let mut swapchain = device.create_swapchain(&mut surface)?;
+    swapchain.configure(
+        sierra::ImageUsage::COLOR_ATTACHMENT,
+        sierra::Format::BGRA8Srgb,
+        sierra::PresentMode::Fifo,
+    )?;
 
     let mut main = Main::instance();
     let pipeline_layout = Pipeline::layout(&device)?;
-    let mut globals = pipeline_layout.globals.instance();
-
-    // let mut graphisc_pipeline = None;
+    let mut graphisc_pipeline = None::<sierra::GraphicsPipeline>;
 
     let bump = bumpalo::Bump::new();
 
@@ -119,29 +70,41 @@ fn main() -> eyre::Result<()> {
 
             winit::event::Event::RedrawRequested(_) => {
                 let result = (|| -> eyre::Result<()> {
-                    let image = loop {
-                        // Note. Hide this loop inside `acquire_image`?
-                        if let Some(image) = swapchain.acquire_image()? {
-                            break image;
-                        }
-
-                        swapchain.configure(
-                            sierra::ImageUsage::COLOR_ATTACHMENT,
-                            sierra::Format::BGRA8Srgb,
-                            sierra::PresentMode::Fifo,
-                        )?;
-                    };
+                    let image = swapchain.acquire_image()?;
+                    let gp;
 
                     let mut encoder = queue.create_encoder(&bump)?;
                     let mut render_pass_encoder = main.begin_render_pass(
                         &Main {
                             target: image.info().image.clone(),
                             bg: sierra::ClearColor(0.3, 0.1, 0.8, 1.0),
-                            depth: sierra::Format::D32Sfloat,
                         },
                         &device,
                         &mut encoder,
                     )?;
+
+                    gp = match &graphisc_pipeline {
+                        Some(gp) if gp.info().render_pass == *render_pass_encoder.render_pass() => {
+                            gp.clone()
+                        }
+                        _ => {
+                            graphisc_pipeline = None;
+                            let extent = render_pass_encoder.framebuffer().info().extent;
+                            let gp =
+                                device.create_graphics_pipeline(sierra::graphics_pipeline!(
+                                    vertex_shader: sierra::VertexShader::new(shader_module.clone(), "vs_main"),
+                                    render_pass: render_pass_encoder.render_pass().clone(),
+                                    layout: pipeline_layout.raw().clone(),
+                                    viewport: extent.into(),
+                                    scissor: extent.into(),
+                                    fragment_shader: Some(sierra::FragmentShader::new(shader_module.clone(), "fs_main")),
+                                ))?;
+
+                            graphisc_pipeline.get_or_insert(gp).clone()
+                        }
+                    };
+                    render_pass_encoder.bind_graphics_pipeline(&gp);
+                    render_pass_encoder.draw(0..3, 0..1);
                     drop(render_pass_encoder);
                     queue.submit(
                         &[(
@@ -153,9 +116,7 @@ fn main() -> eyre::Result<()> {
                         None,
                         &bump,
                     );
-
                     queue.present(image)?;
-
                     Ok(())
                 })();
 
@@ -166,89 +127,4 @@ fn main() -> eyre::Result<()> {
             _ => {}
         }
     })
-}
-
-pub fn example(
-    device: &sierra::Device,
-    queue: &mut sierra::Queue,
-    main: &Main,
-    fence: usize,
-    globals: &Globals,
-    objects: &mut Vec<(&Object, Option<ObjectInstance>)>,
-    mut graphics_pipeline_info: ::sierra::GraphicsPipelineInfo,
-    bump: &bumpalo::Bump,
-) -> Result<(), sierra::FramebufferError> {
-    let graphics_pipeline;
-
-    // Create pipeline layout
-    let mut main_instance = Main::instance();
-
-    // Create globals instance
-    let pipeline_layout = Pipeline::layout(device)?;
-    let mut globals_instance = pipeline_layout.globals.instance();
-
-    // The following should be repeated each frame.
-
-    // Make vector to store descriptor writes.
-    let mut writes = bumpalo::collections::Vec::new_in(bump);
-
-    // Create encoder to encode commands before render pass.
-    let mut encoder = queue.create_encoder(bump)?;
-
-    // Update globals.
-    // This may extend descriptors writes and record some commands.
-    let globals = globals_instance.update(globals, fence, device, &mut writes, &mut encoder)?;
-
-    // Begin render pass encoding in parallel.
-    let mut render_pass_encoder = queue.create_encoder(bump)?;
-    let mut render_pass = render_pass_encoder.with_render_pass(&mut main_instance, main, device)?;
-
-    // Finish creating graphics pipeline
-    graphics_pipeline_info.layout = pipeline_layout.raw().clone();
-    graphics_pipeline_info.render_pass = render_pass.render_pass().clone();
-    graphics_pipeline = device.create_graphics_pipeline(graphics_pipeline_info)?;
-
-    // Don't forget to bind graphics pipeline.
-    render_pass.bind_graphics_pipeline(&graphics_pipeline);
-
-    // Bind globals to graphics pipeline
-    pipeline_layout.bind_graphics(globals, &mut render_pass);
-
-    for (object, instance) in objects.iter_mut() {
-        // Ensure object descriptors instance is attached to each object
-        let instance = match instance {
-            Some(instance) => instance,
-            slot => slot.get_or_insert(pipeline_layout.object.instance()),
-        };
-
-        // Update object descriptors
-        let object = instance.update(object, fence, device, &mut writes, &mut encoder)?;
-
-        // Bind object descriptors to graphics pipeline.
-        pipeline_layout.bind_graphics(object, &mut render_pass);
-
-        // Currently vertices and instances binding is not covered by sierra's code-gen.
-        // Here's dummy values.
-        let vertices = 0..0;
-        let instances = 0..0;
-
-        render_pass.draw(vertices, instances);
-    }
-
-    // Render pass recording ends here.
-    drop(render_pass);
-
-    // Ensure to flush descriptors writes before submitting commands.
-    device.update_descriptor_sets(&writes, &[]);
-
-    // Submit commands.
-    queue.submit(
-        &[],
-        std::array::IntoIter::new([encoder.finish(), render_pass_encoder.finish()]),
-        &[],
-        None,
-        bump,
-    );
-
-    Ok(())
 }
